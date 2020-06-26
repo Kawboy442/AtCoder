bdd3816eac2538e2b70994633c18bb649f8b0bb4
R.Ito <kawboy442@gmail.com>
2020-06-26 22:12:33 +0900

    初回コミット

---
+++ AtCoder/AtCoder/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AtCoder
+//
+//  Created by 伊藤僚亮 on 2020/06/26.
+//  Copyright © 2020 伊藤僚亮. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
